3e9aca80437665f3b37819818b73d73fe2989ae0
Hirako2000 <hirako2000@gmail.com>
2017-07-06 21:44:03 +0700

    Adds cross-env, adds compression, fix build script, separate chunks for vendor and app, use html plugin, cleanup how plugins are listed

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,11 +1,15 @@
 import path from 'path';
 import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CompressionPlugin from 'compression-webpack-plugin';
 import alias from 'whs/tools/alias';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
 const config = {
-  entry: './app/app.js',
+  entry: {
+    app: './app/app.js',
+  },
 
   module: {
     rules: [
@@ -32,32 +36,70 @@ const config = {
     ]
   },
 
-  plugins: isProduction
-    ? [
-      new webpack.LoaderOptionsPlugin({
-        minimize: true
-      }),
-      new webpack.optimize.UglifyJsPlugin(),
-      new webpack.optimize.ModuleConcatenationPlugin()
-    ]
-    : [new webpack.optimize.ModuleConcatenationPlugin()],
+  plugins: pluginsList(isProduction),
 
   output: {
     path: path.join(__dirname, './build/'),
-    filename: 'bundle.js'
+    filename: '[name].[chunkhash].js'
   },
 
   devServer: {
-    publicPath: '/build/',
+    publicPath: '/',
     stats: { chunks: true }
   },
 
   resolve: {
-    alias,
     symlinks: false,
     modules: [path.resolve('node_modules')]
   }
 };
+
+function pluginsList(prod) {
+  const plugins = [
+      new webpack.optimize.ModuleConcatenationPlugin(),
+      new webpack.optimize.CommonsChunkPlugin({
+        names: ['vendor'],
+        chunks: ['app'],
+        minChunks: function (module) {
+          return isExternal(module)
+        }
+      }),
+      new webpack.NamedModulesPlugin(),
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        title: 'whs boilerplate app',
+        template: 'index-template.html',
+        chunks: ['vendor', 'app']
+      }),
+      new CompressionPlugin({
+        asset: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: /\.js$|\.css$|\.html$/,
+        threshold: 1024,
+        minRatio: 0.8
+      })
+  ];
+  if(prod) {
+    plugins.push(
+      new webpack.LoaderOptionsPlugin({
+        minimize: true
+      }),
+      new webpack.optimize.UglifyJsPlugin())
+  }
+  return plugins;
+}
+
+function isExternal(module) {
+  const userRequest = module.userRequest
+
+  if (typeof userRequest !== 'string') {
+    return false
+  }
+
+  return userRequest.indexOf('bower_components') >= 0 ||
+         userRequest.indexOf('node_modules') >= 0 ||
+         userRequest.indexOf('libraries') >= 0
+}
 
 export {
   config as default
